11d10a1a521cb9f33beb0df7e857861f333081cd
xocialize <dustin.nielson@gmail.com>
2023-05-06 11:38:58 -0700

    Update CapturePreviewView.swift

--- Sources/CaptureManager/CapturePreviewView.swift
+++ Sources/CaptureManager/CapturePreviewView.swift
@@ -5,11 +5,17 @@
 //  Created by Dustin Nielson on 9/2/22.
 //
 
+
+#if os(iOS)
+import UIKit
+#else
 import Cocoa
+#endif
 import AVFoundation
 
-class CapturePreviewView: NSView {
-    
+#if os(iOS)
+class CapturePreviewView: UIView {
+
     
     override func draw(_ dirtyRect: NSRect) { super.draw(dirtyRect) }
     
@@ -32,3 +38,30 @@ class CapturePreviewView: NSView {
     }
     
 }
+#else
+class CapturePreviewView: NSView {
+
+    
+    override func draw(_ dirtyRect: NSRect) { super.draw(dirtyRect) }
+    
+    required init?(coder: NSCoder) { super.init(coder: coder) }
+    
+    override init(frame frameRect: NSRect) {
+        super.init(frame: frameRect)
+        self.wantsLayer = true
+        layer?.masksToBounds = true
+        layer?.autoresizingMask = [.layerWidthSizable, .layerHeightSizable]
+    }
+    
+    func initPreviewLayer(videoPreviewLayer: AVCaptureVideoPreviewLayer) {
+        self.layer!.insertSublayer(videoPreviewLayer, at: 0)
+        videoPreviewLayer.frame = self.frame
+    }
+    
+    func removePreviewLayer() {
+        self.layer?.sublayers?.removeAll()
+    }
+    
+}
+#endif
+
